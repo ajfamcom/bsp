307d64ae061c1a9d66ddf0c77a8fd75fc42c23c0
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-21 00:43:58 -0700

    files changed

--- wp-content/themes/twentytwentyone/template-parts/header/site-header.php
+++ wp-content/themes/twentytwentyone/template-parts/header/site-header.php
@@ -14,9 +14,8 @@ $wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
 ?>
 
 <header id="masthead" class="<?php echo esc_attr( $wrapper_classes ); ?>">
-   
-    <?php get_template_part( 'template-parts/news-header-section' );?>   
-	<?php get_template_part( 'template-parts/header/site-branding' ); ?><img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.svg">
+
+	<?php get_template_part( 'template-parts/header/site-branding' ); ?>
 	<?php get_template_part( 'template-parts/header/site-nav' ); ?>
-    
+
 </header><!-- #masthead -->
--- wp-content/themes/twentytwentyone/template-parts/home-top-section.php
+++
@@ -1,4 +0,0 @@
-<?php /**Template Name:Home Top Section */ ?>
-<div class="row">  
-<?php do_shortcode('[home_top]'); ?>
-</div>
