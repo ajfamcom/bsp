1d08ba21574d426b23aa5692aac69589a00decca
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-21 03:20:32 -0700

    Update header.php

--- wp-content/themes/bspblog/header.php
+++ wp-content/themes/bspblog/header.php
@@ -1,14 +1,5 @@
 <?php
-/**
- * The header for Astra Theme.
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Astra
- * @since 1.0.0
- */
+
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -18,17 +9,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php astra_html_before(); ?>
 <html <?php language_attributes(); ?>>
 <head>
-<?php astra_head_top(); ?>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<?php 
-if ( apply_filters( 'astra_header_profile_gmpg_link', true ) ) {
-	?>
-	 <link rel="profile" href="https://gmpg.org/xfn/11"> 
-	 <?php
-} 
-?>
-<?php wp_head(); ?>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/custom-style.css" type="text/css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<?php wp_head(); ?>
 <?php astra_head_bottom(); ?>
 </head>
 
