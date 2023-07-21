8c5c44575da21220ea80faa0d3909c46c092908c
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-21 17:14:44 +0530

    header formatting..

--- wp-content/themes/bspblog/template-parts/news-header-section.php
+++ wp-content/themes/bspblog/template-parts/news-header-section.php
@@ -2,16 +2,14 @@
 
 /**Template Name:News Header Section */ ?>
 <div class="latest-news-top-section">
+    <div class="container">
     <div class="row">
-    <div class="col-md-6">
-        <div class="row">
+        <div class="col-md-6">
             <?php do_shortcode('[news_header]'); ?>
         </div>
-    </div>
-    <div class="col-md-6">
-        <div class="row">
+        <div class="col-md-6">
             <?php include "searchform.php"; ?>
         </div>
     </div>
-</div>
+    </div>
 </div>
