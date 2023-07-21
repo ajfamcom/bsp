c1fc9a889f7a339f1a26e2bd66684b0c15f03c2e
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-21 18:01:52 +0530

    redo
    redo

--- wp-content/themes/bspblog/template-parts/news-header-section.php
+++ wp-content/themes/bspblog/template-parts/news-header-section.php
@@ -8,7 +8,7 @@
             <div class="head-news"><?php do_shortcode('[news_header]'); ?></div>
         </div>
         <div class="col-md-6">
-            <?php include "searchform.php"; ?>
+        <div class="head-search"><?php include "searchform.php"; ?></div>
         </div>
     </div>
     </div>
