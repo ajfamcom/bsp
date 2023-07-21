4fedb490d8036cae99b45796f9b872b8580d1717
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-21 03:15:32 -0700

    changes

--- wp-content/themes/bspblog/searchform.php
+++ wp-content/themes/bspblog/searchform.php
@@ -1,39 +1,29 @@
 <?php
 /**
- * Search Form for Astra theme.
+ * The searchform.php template.
  *
- * @package     Astra
- * @author      Brainstorm Force
- * @copyright   Copyright (c) 2020, Brainstorm Force
- * @link        https://www.brainstormforce.com
- * @since       Astra 3.3.0
+ * Used any time that get_search_form() is called.
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_unique_id/
+ * @link https://developer.wordpress.org/reference/functions/get_search_form/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
  */
 
-/**
- * Adding argument checks to avoid rendering search-form markup from other places & to easily use get_search_form() function.
- *
- * @see https://themes.trac.wordpress.org/ticket/101061
- * @since 3.6.1
+/*
+ * Generate a unique ID for each form and a string containing an aria-label
+ * if one was passed to get_search_form() in the args array.
  */
-$astra_search_input_placeholder = isset( $args['input_placeholder'] ) ? $args['input_placeholder'] : astra_default_strings( 'string-search-input-placeholder', false );
-$astra_search_show_input_submit = isset( $args['show_input_submit'] ) ? $args['show_input_submit'] : true;
-$astra_search_data_attrs        = isset( $args['data_attributes'] ) ? $args['data_attributes'] : '';
-$astra_search_input_value       = isset( $args['input_value'] ) ? $args['input_value'] : '';
+$twentytwentyone_unique_id = wp_unique_id( 'search-form-' );
 
+$twentytwentyone_aria_label = ! empty( $args['aria_label'] ) ? 'aria-label="' . esc_attr( $args['aria_label'] ) . '"' : '';
 ?>
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label>
-		<span class="screen-reader-text"><?php echo esc_html__( 'Search for:', 'astra' ); ?></span>
-		<input type="search" class="search-field" <?php echo esc_attr( $astra_search_data_attrs ); ?> placeholder="<?php echo esc_attr( $astra_search_input_placeholder ); ?>" value="<?php echo esc_attr( $astra_search_input_value ); ?>" name="s" tabindex="-1">
-		<?php if ( class_exists( 'Astra_Icons' ) && Astra_Icons::is_svg_icons() ) { ?>
-			<button class="search-submit ast-search-submit" aria-label="<?php echo esc_attr__( 'Search Submit', 'astra' ); ?>">
-				<span hidden><?php echo esc_html__( 'Search', 'astra' ); ?></span>
-				<i><?php Astra_Icons::get_icons( 'search', true ); ?></i>
-			</button>
-		<?php } ?>
-	</label>
-	<?php if ( $astra_search_show_input_submit ) { ?>
-		<input type="submit" class="search-submit" value="<?php echo esc_attr__( 'Search', 'astra' ); ?>">
-	<?php } ?>
+<form role="search" <?php echo $twentytwentyone_aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?> method="get" class="search-form" action="#.">
+	<label for="<?php echo esc_attr( $twentytwentyone_unique_id ); ?>"><?php _e( 'Search&hellip;', 'twentytwentyone' ); // phpcs:ignore: WordPress.Security.EscapeOutput.UnsafePrintingFunction -- core trusts translations ?></label>
+	<input type="search" id="search-data" class="search-field" value="<?php echo get_search_query(); ?>" name="s" />
+	<input type="button" id="search-data-btn" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'twentytwentyone' ); ?>" style="background:black;padding:13px;color:white;font-size:20px;cursor:pointer;"/>
 </form>
-<?php
+<div>Search Result:</div>
+<div id="result-data"></div>
