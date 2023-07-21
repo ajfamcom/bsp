2adf727b1b55dcf8491adbd29a1321755f96d8b5
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-21 01:18:45 -0700

    file changes

--- wp-content/themes/twentytwentyone/footer.php
+++ wp-content/themes/twentytwentyone/footer.php
@@ -16,67 +16,32 @@
 		</div><!-- #primary -->
 	</div><!-- #content -->
 
-	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
-
-	<footer id="colophon" class="site-footer">
-
-		<?php if ( has_nav_menu( 'footer' ) ) : ?>
-			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
-				<ul class="footer-navigation-wrapper">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</ul><!-- .footer-navigation-wrapper -->
-			</nav><!-- .footer-navigation -->
-		<?php endif; ?>
-		<div class="site-info">
-			<div class="site-name">
-				<?php if ( has_custom_logo() ) : ?>
-					<div class="site-logo"><?php the_custom_logo(); ?></div>
-				<?php else : ?>
-					<?php if ( get_bloginfo( 'name' ) && get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
-						<?php if ( is_front_page() && ! is_paged() ) : ?>
-							<?php bloginfo( 'name' ); ?>
-						<?php else : ?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						<?php endif; ?>
-					<?php endif; ?>
-				<?php endif; ?>
-			</div><!-- .site-name -->
-
-			<?php
-			if ( function_exists( 'the_privacy_policy_link' ) ) {
-				the_privacy_policy_link( '<div class="privacy-policy">', '</div>' );
-			}
-			?>
-
-			<div class="powered-by">
-				<?php
-				printf(
-					/* translators: %s: WordPress. */
-					esc_html__( 'Proudly powered by %s.', 'twentytwentyone' ),
-					'<a href="' . esc_url( __( 'https://wordpress.org/', 'twentytwentyone' ) ) . '">WordPress</a>'
-				);
-				?>
-			</div><!-- .powered-by -->
-
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-
+	
+ <!--footer area-->
+	<footer id="colophon" class="custom-site-footer ">
+     <div class="col-md-12">
+		<div class="row">
+			<p>BSP Research</p>
+			<p>Start working with our team today</p>
+			<small>Interested in polling or message testing research?Focus groups or qualitative interviews?Modeling and data analytics?Contact us today to discuss the possibilites...</small>
+		</div>
+     </div>	 
+	 </footer>
+	 <div class="col-md-12">
+		<div class="row">
+		<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
+         </div>
+		 <div class="row">Copyright@ 2023 BSP Research _all Rights Reserved.</div>
+     </div>
+	 <!--footer area-->
 </div><!-- #page -->
-<!--jquery files-->
+
+<?php wp_footer(); ?>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script>
+	/*
 $(document).ready(function(){
 	$('#search-data-btn').on('click',function(){
 		
@@ -127,9 +92,7 @@ $(document).ready(function(){
 	
 	
 	})
-});
+});*/
 </script>
-<?php wp_footer(); ?>
-
 </body>
 </html>
--- wp-content/themes/twentytwentyone/home.php
+++ wp-content/themes/twentytwentyone/home.php
@@ -1,40 +1,41 @@
 <?php
-/* Template Name: Home */
-
+/**
+ * Template Name:Home
+ */
 get_header(); ?>
-
-<div>SLIDER IMAGES</div>
-
-<div>About Us Content</div>
-
-<div>Recent Research</div>
-
-<div>
-	<h3>Services and expertise</h3>
-<?php if ( is_active_sidebar( 'services-expertise-area' ) ) : ?>
-    <div class="custom-widget-area">
-        <?php dynamic_sidebar( 'services-expertise-area' ); ?>
+<div class="container ">
+<div class="top-header-block col-md-12 d-flex justify-content-center text-center" style="text-align:center;" >
+	    <?php	
+			get_template_part( 'template-parts/home-top-section' );
+		?>
+</div>
+<div class="top-header-block col-md-12 d-flex justify-content-center text-center" style="text-align:center;" >
+	    <?php	
+			get_template_part( 'template-parts/services-and-expertise' );
+		?>
+</div>
+<div class="most-popular-block">
+	<div class="col-md-8">
+		<div class="row">			
+		<?php	
+			get_template_part( 'template-parts/most-popular-posts' );
+		?>
+		</div>
     </div>
-<?php endif; ?>
-
+    <div class="col-md-4">
+		<?php	
+			get_template_part( 'template-parts/news-and-analysis' );
+		?>
+    </div>
 </div>
-
-<div><h3>Team Section</h3>
-<?php
-$page_id = 11; // Replace 123 with the ID of the page you want to retrieve data from
-
-$page_data = get_page($page_id); // Retrieve the page data
-
-if ($page_data) {
-    $page_title = $page_data->post_title; // Get the page title
-    $page_content = apply_filters('the_content', $page_data->post_content); // Get the page content
-    
-    // Output the page title and content
-    echo '<h1>' . $page_title . '</h1>';
-    echo '<div>' . $page_content . '</div>';
-}
-?>
+<div class="team-members-block  d-flex justify-content-center">
+	    <?php	
+			get_template_part( 'template-parts/team-members' );
+		?>
 </div>
-<?php
-
+</div>
+<?php 
 get_footer();
+?>
+
+
