d0f437165d01f09d7174dd099416591d1ffb41c2
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-21 03:48:01 -0700

    Update footer.php

--- wp-content/themes/bspblog/footer.php
+++ wp-content/themes/bspblog/footer.php
@@ -30,27 +30,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</div><!-- #page -->
 <?php 
 	astra_body_bottom();    
-	//wp_footer(); 
+	wp_footer(); 
 ?>
-<footer id="colophon" class="custom-site-footer ">
-     <div class="col-md-12">
-		<div class="row">
-			<p>BSP Research</p>
-			<p>Start working with our team today</p>
-			<small>Interested in polling or message testing research?Focus groups or qualitative interviews?Modeling and data analytics?Contact us today to discuss the possibilites...</small>
-		</div>
-     </div>	 
-	 </footer>
-	 <div class="col-md-12">
-		<div class="row">
-		<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
-         </div>
-		 <div class="row">Copyright@ 2023 BSP Research _all Rights Reserved.</div>
-     </div>
-	 <!--footer area-->
-</div><!-- #page -->
-
-<?php //wp_footer(); ?>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
