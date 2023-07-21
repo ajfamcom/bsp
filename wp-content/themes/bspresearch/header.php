cd70f9d0d8b96a262e3e77543c3b2779d5348dde
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-21 11:39:49 +0530

    home page container formatting
    home page container formatting

--- wp-content/themes/bspresearch/header.php
+++ wp-content/themes/bspresearch/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/custom-style.css" type="text/css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<?php wp_head(); ?>
 </head>
@@ -10,7 +11,7 @@
 
 
 <?php wp_body_open(); ?>
-<div id="page" class="site container">
+<div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content">
 		<?php
 		/* translators: Hidden accessibility text. */
