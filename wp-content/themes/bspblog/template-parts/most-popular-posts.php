f61394547f26ab10e7752357a40bb21f0bbd7fc8
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-24 03:58:45 -0700

    Update most-popular-posts.php

--- wp-content/themes/bspblog/template-parts/most-popular-posts.php
+++ wp-content/themes/bspblog/template-parts/most-popular-posts.php
@@ -50,10 +50,50 @@ if ($query->have_posts()) {
 // Reset the query to avoid conflicts with other queries
  
 ?>  
-<div class="static-post"><img src=""><>Georgios Political power grows</h3>
-<p class="post-date">Sep 7, 2023</p>
+<?php
+$args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 1,    
+    'meta_query' => array(
+        array(
+            'key' => 'is_static_post', 
+            'value'   => 'Yes', // Serialized value for 'Yes'
+            'compare' => 'LIKE'
+        ),
+    ),
+);
 
-</div>
+$query = new WP_Query( $args );
+
+if ($query->have_posts()) {
+    while ($query->have_posts()) {
+        $query->the_post();
+        $post_id = get_the_ID();
+       
+        $post_date = get_the_date( 'M j, Y', $post_id );
+        ?>
+   
+    <div class="static-post">
+    <div class="static-post-img">
+	<?php
+    if (has_post_thumbnail()) {
+    the_post_thumbnail(); 
+    }
+    ?>
+    </div>
+    <div class="static-post-discription">
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	
+    <p class="post-date"><?php echo $post_date;?></p>
+    </div>
+    </div>
+   
+        <?php
+    }
+ wp_reset_postdata();
+    
+}
+?>
 </div>
 <div class="col-md-5">
 <?php
