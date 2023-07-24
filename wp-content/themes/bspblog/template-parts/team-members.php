34e344866bcc15e942d535b4f9f74449ce9e8515
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-24 14:44:17 +0530

    redo
    redo

--- wp-content/themes/bspblog/template-parts/team-members.php
+++ wp-content/themes/bspblog/template-parts/team-members.php
@@ -37,13 +37,13 @@
             $image = get_field('profile_image', $post_id);
             ?> 
 <div class="single-team-member col-md-3">
-    <p class="member-image"><img src="<?php echo $image['url'];?>" width="200px;"/></p>
+    <img class="member-image" src="<?php echo $image['url'];?>" width="200px;"/>
     <h4 class="member-details"><span class="member-name"><?php echo $fullname; ?></span>,<span class="member-education"><?php echo $education;?></span></h4>
     <p class="other-details"><span class="member-position"><?php echo $designation;?></span></p>
     <!-- <p class="bio"><?php the_content(); ?></p> -->
 </div>
 <?php endwhile; ?>
 <?php endif;?>
-<div class="col-12 text-center"><p><a href="#."><button type="button" class="btn btn-primary">MEET THE TEAM</button></a></p></div>
+<div class="col-12 text-center"><a href="#."><button type="button" class="btn btn-primary">MEET THE TEAM</button></a></div>
 </div>
 
