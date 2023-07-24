92e540b6a610119b91221c2aaf449c5b814b2269
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-24 16:04:07 +0530

    redo
    redo

--- wp-content/themes/bspblog/template-parts/team-members.php
+++ wp-content/themes/bspblog/template-parts/team-members.php
@@ -66,6 +66,6 @@
 </div>
 <?php endwhile; ?>
 <?php endif;?>
-<div class="col-12 text-center"><a href="#."><button type="button" class="btn btn-primary">MEET THE TEAM</button></a></div>
+<div class="col-12 text-center pt-4"><a href="#."><button type="button" class="btn btn-primary">MEET THE TEAM</button></a></div>
 </div>
 
