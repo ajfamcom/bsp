4c6059b9c81b6545556aa7418295dbc436f0702c
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-24 17:42:28 +0530

    news section design work.
    news section design work.

--- wp-content/themes/bspblog/home.php
+++ wp-content/themes/bspblog/home.php
@@ -22,7 +22,7 @@ get_header(); ?>
 
 <div class="container pb-5 mb-5">
 <div class="most-popular-block pb-md-4">
-	<div class="row">
+	<div class="row gx-5">
 	<div class="col-md-9">		
 		<div class="row">
 			<?php	
