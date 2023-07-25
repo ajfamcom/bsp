979010f12511c6d7e86326f4570f286328521e5b
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-24 21:49:40 -0700

    Update functions.php

--- wp-content/themes/bspblog/functions.php
+++ wp-content/themes/bspblog/functions.php
@@ -312,7 +312,7 @@ $result =$wpdb->get_results($mquery);
     }
 	if (is_search() && $query->is_main_query() && empty($query->query_vars['s'])) {
         // Set the search query to an arbitrary value to prevent results from showing
-        $query->set('s', 'no_results');
+        $query->set('s', '00000');
         
         // Set the search order to none to avoid executing the search query
         $query->set('order', 'none');
