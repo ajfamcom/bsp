86ca3436602422fb9408e790c7a03d196ded3e47
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-25 17:06:11 +0530

    mobile header files
    mobile header files

--- wp-content/themes/bspblog/template-parts/mobile-header-section.php
+++ wp-content/themes/bspblog/template-parts/mobile-header-section.php
@@ -4,13 +4,11 @@
 <div class="latest-news-top-section">
     <div class="container">
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-4 mobile-btns">
             <a href="<?php echo site_url('contact-us');?>">Contact Us</a>
-        </div>
-        <div class="col-md-4">
             <a href="#." class="subscribe-btn">Subscribe Us</a>
         </div>
-        <div class="col-md-4">
+        <div class="col-8">
         <div class="head-search"><?php include "searchform.php"; ?></div>
         </div>
     </div>
