1a70cf3c104bdba8f190adfb3264c135a6949c52
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-25 18:21:44 +0530

    mobile formatting
    mobile formatting

--- wp-content/themes/bspblog/template-parts/mobile-header-section.php
+++ wp-content/themes/bspblog/template-parts/mobile-header-section.php
@@ -4,11 +4,11 @@
 <div class="latest-news-top-section">
     <div class="container">
     <div class="row">
-        <div class="col-5 mobile-btns">
+        <div class="col-6 mobile-btns">
             <a href="<?php echo site_url('contact-us');?>">Contact Us</a>
             <a href="#." class="subscribe-btn">Subscribe Us</a>
         </div>
-        <div class="col-7">
+        <div class="col-6">
         <div class="head-search"><?php include "searchform.php"; ?></div>
         </div>
     </div>
