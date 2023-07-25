435c42f147a3850dbe27984ea121e7c7981be8fc
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-24 21:50:53 -0700

    Update functions.php

--- wp-content/themes/bspblog/functions.php
+++ wp-content/themes/bspblog/functions.php
@@ -312,10 +312,12 @@ $result =$wpdb->get_results($mquery);
     }
 	if (is_search() && $query->is_main_query() && empty($query->query_vars['s'])) {
         // Set the search query to an arbitrary value to prevent results from showing
-        $query->set('s', '00000');
+        $query->set('s', '');
         
         // Set the search order to none to avoid executing the search query
         $query->set('order', 'none');
+
+		return '';
     }
     
 }
