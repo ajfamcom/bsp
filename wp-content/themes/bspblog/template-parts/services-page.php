48cc5514419785ea94f7e348ac6ef65340910a30
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-26 16:44:38 +0530

    services page code
    services page code

--- wp-content/themes/bspblog/template-parts/services-page.php
+++ wp-content/themes/bspblog/template-parts/services-page.php
@@ -3,7 +3,7 @@
 
 ?>
 <?php get_header(); ?>
-<div class="inner-bnr about-bnr">
+<div class="inner-bnr services-bnr">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-8 col-sm-8 col-12">
@@ -15,7 +15,7 @@
 				</div>
 			</div>
 			<div class="col-md-4 col-sm-4 col-12">
-				<img class="img-fluid bnr-simg" src="<?php bloginfo('template_directory'); ?>/assets/images/about-side-bnrimg.png" alt="side-bnrimg">
+				<img class="img-fluid bnr-simg" src="<?php bloginfo('template_directory'); ?>/assets/images/services-side-bnrimg.png" alt="side-bnrimg">
 			</div>
 		</div>
 	</div>
@@ -23,62 +23,14 @@
 
 <div class="container py-5 my-md-5">
 	<div class="row py-md-5 my-md-5">
-		<div class="col-md-5 col-sm-5 col-12">
-			<div class="image-group">
-				<img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/images/about-img1.png" alt="bodyimage">
-				<img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/images/about-img2.png" alt="bodyimage">
-			</div>
-		</div>
-		<div class="col-md-7 col-sm-7 col-12">
+		<div class="col-md-6 col-sm-6 col-12">
 			<h2>Cultural Competence in Today's America</h2>
 			<p>For the past 20 years, we have helped political, corporate and community-based organizations understand a diverse America, and craft the best messages to communicate. Our process is designed to work with you, solicit your input, so we can empower your organization with the specific and customized messages, data and tools needed to succeed. Our team deeply understands the diversity that is America today.</p>
 			<h2>Our Analytical Approach</h2>
 			<p>Bad data is worse than just guessing, it leads you to the wrong conclusions. Our first priority is to make sure we have accurate, culturally competent and representative data to inform our models and analysis. Today, there is vast variability in the quality of data, especially when it comes to Latinos, immigrants and under-represented Americans. Do not accept low quality data to inform your project. Our data scientists have quite literally written the book on research methodology, modeling and data analytics of communities of color.</p>
 		</div>
-	</div>
-</div>
-
-<div class="track-record-section">
-	<div class="container py-5">
-		<div class="row">
-			<div class="col-12 text-center">
-				<h1>A Proven Track Record</h1>
-				<p class="subtitle">We work with a large number of clients from various industries, including:</p>
-			</div>
-		</div>
-
-		<div class="row">
-
-			<div class="col-md-3 col-sm-4 col-12 my-2 my-md-4">
-				<div class="track-div">
-					<i class="fa-regular fa-circle-check"></i>
-					<h4>Political Campaigns</h4>
-				</div>
-			</div>
-			<div class="col-md-3 col-sm-4 col-12 my-2 my-md-5">
-				<div class="track-div">
-					<i class="fa-regular fa-circle-check"></i>
-					<h4>Issue Advocacy Groups</h4>
-				</div>
-			</div>
-			<div class="col-md-3 col-sm-4 col-12 my-2 my-md-4">
-				<div class="track-div">
-					<i class="fa-regular fa-circle-check"></i>
-					<h4>Civic Engagement Orgs</h4>
-				</div>
-			</div>
-			<div class="col-md-3 col-sm-4 col-12 my-2 my-md-5">
-				<div class="track-div">
-					<i class="fa-regular fa-circle-check"></i>
-					<h4>Media</h4>
-				</div>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="col-12 text-center">
-				<p class="mb-0">Our political experts are ready assist you in developing a plan that drives helps you achieve your goals. In addition to delivering high quality research to our clients, we can work directly with our extensive political and media contacts to make sure your results penetrate the bubble and help inform the highest level decisions makers.</p>
-			</div>
+		<div class="col-md-6 col-sm-6 col-12">
+				<img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/images/services-pageimg.png" alt="bodyimage">
 		</div>
 	</div>
 </div>
