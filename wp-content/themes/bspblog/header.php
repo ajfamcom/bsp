87f966373a3e44b23e4aee694c06f757a8fca0a7
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-26 15:35:40 +0530

    redo
    redo

--- wp-content/themes/bspblog/header.php
+++ wp-content/themes/bspblog/header.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/custom_style.css" type="text/css">
-	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/media_queries.css" type="text/css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/media-queries.css" type="text/css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<?php wp_head(); ?>
 <?php astra_head_bottom(); ?>
