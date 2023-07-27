e679c3def149856f5c3656f0bcc47fe7dec9c601
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-27 16:18:32 +0530

    single page team member page
    single page team member page

--- wp-content/themes/bspblog/single-team_members.php
+++ wp-content/themes/bspblog/single-team_members.php
@@ -1,24 +1,24 @@
 <?php
 /* Template Name:Custom Team Members Template  */
-$page_id =get_custom_page_id('team_members','our-team');
+$page_id = get_custom_page_id('team_members', 'our-team');
 $full_banner = get_field('full_banner', $page_id);
 $image_over_banner = get_field('image_over_banner', $page_id);
 if (have_posts()) :
-    while (have_posts()) : the_post();
-        $post_id = get_the_ID();		
-						$fullname = get_field('full_name', $post_id);
-						$education = get_field('education', $post_id);
-						$designation = get_field('designation', $post_id);
-						$image = get_field('profile_image', $post_id);
-    endwhile;
-endif;					
+	while (have_posts()) : the_post();
+		$post_id = get_the_ID();
+		$fullname = get_field('full_name', $post_id);
+		$education = get_field('education', $post_id);
+		$designation = get_field('designation', $post_id);
+		$image = get_field('profile_image', $post_id);
+	endwhile;
+endif;
 ?>
-<?php get_header();?>
+<?php get_header(); ?>
 
-<div class="inner-bnr team-bnr" style="background-image: linear-gradient(180deg, rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url('<?php echo $full_banner['url']; ?>')">
+<div class="single-inner-bnr">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-8 col-sm-8 col-12">
+			<div class="col-12">
 				<div class="row page-banner">
 					<?php echo get_breadcrumbs(); ?>
 					<div class="page-title">
@@ -26,28 +26,25 @@ endif;
 					</div>
 				</div>
 			</div>
-			<div class="col-md-4 col-sm-4 col-12">
-				<img class="img-fluid bnr-simg" src="<?php echo $image_over_banner['url'];?>" alt="side-bnrimg">
-			</div>
 		</div>
 	</div>
 </div>
 
-<div class="container">	
+<div class="container">
 	<div class="col-md-12 py-5">
-		<div class="row">			
-							<div class="single-team-member col-md-4">
-                            <h4 class="member-details"><span class="member-name"><?php echo $fullname; ?></span>,<span class="member-education"><?php echo $education;?></span></h4>
-							<div class="member-image-square"><img src="<?php echo $image['url'];?>" /></div></div>
-                            <div class="single-team-member col-md-8">								
-								<p class="other-details"><span class="member-position"><?php echo $designation;?></span></p>
-								<p class="bio"><?php the_content();?></p>
-							</div>
-					
-        </div>
-		
-    </div>
-</div>	
-<?php 
+		<div class="row">
+
+			<div class="single-team-member col-12">
+				<div class="-single-member-image"><img src="<?php echo $image['url']; ?>" /></div>
+				<h4 class="member-details"><span class="member-name"><?php echo $fullname; ?></span>,<span class="member-education"><?php echo $education; ?></span></h4>
+				<p class="other-details"><span class="member-position"><?php echo $designation; ?></span></p>
+				<p class="bio"><?php the_content(); ?></p>
+			</div>
+
+		</div>
+
+	</div>
+</div>
+<?php
 get_footer();
 ?>
