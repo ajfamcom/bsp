4117cc711d87233b861482cf204c951007ad8ff4
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-26 21:17:26 -0700

    service page

--- wp-content/themes/bspblog/template-parts/services-page.php
+++ wp-content/themes/bspblog/template-parts/services-page.php
@@ -24,14 +24,28 @@
 <div class="container py-5 my-md-5">
 	<div class="row align-items-center py-md-5 my-md-5">
 		<div class="col-md-6 col-sm-6 col-12">
-			<h2>Public Opinion Polling</h2>
-			<p>Whether you are interested in a statewide sample or drilling down in Latinos, our firm has expertise in sample, mixed-mode methodology, and helping you understand and analyze public opinion</p>
-			<h2>Message and Ad Testing</h2>
-			<p>After baseline attitudes are established, we can help you communicate most effectively with the target audience through survey experiments and message testing that evaluate how different themes or advertisements resonate for your respondents as a whole, but also within key subgroups within the population</p>
-			<h2>Data Analytics and Modeling</h2>
-			<p>Big data can be overwhelming. Our team of data scientists have pioneered some of the leading methodology and statistical techniques to best assess large datasets and distill them into digestible patterns</p>
-			<h2>Focus Groups and Qualitative</h2>
-			<p class="mb-5 mb-md-0">Whether it is at the start of a new project, or to wrap up a quantitative study, having in-depth conversations with everyday people is crucial to telling the full story and going beyond numbers. We have extensive experience with qualitative research, including focusing in on diverse and underrepresented populations.</p>
+		<?php
+$args = array(
+    'post_type' => 'manage_services',
+    'posts_per_page' => 4,
+    'meta_key'       => 'services_sort_order', // New parameter for sorting
+    'orderby'        => 'meta_value_num',    // Sort by numeric value of meta_key
+    'order'          => 'ASC',              // Ascending order
+);
+$query = new WP_Query( $args );
+if ($query->have_posts()) {
+    while ($query->have_posts()) {
+        $query->the_post();
+        $post_id = get_the_ID();
+        
+?>
+			<h2><?php the_title(); ?></h2>
+			<p><?php the_content();?></p>
+			<?php
+    }
+ wp_reset_postdata();    
+} 
+?>  			
 		</div>
 		<div class="col-md-6 col-sm-6 col-12">
 				<img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/images/services-pageimg.png" alt="bodyimage">
