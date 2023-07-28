0e71fd5fe0d61590cd42019c93fd0a3b029897f5
Rohit_FamCom <106315193+Rohit-Bhadana@users.noreply.github.com>
2023-07-28 12:42:13 +0530

    contact page formatting
    contact page formatting

--- wp-content/themes/bspblog/template-parts/contact-us.php
+++ wp-content/themes/bspblog/template-parts/contact-us.php
@@ -23,17 +23,16 @@
 
 <div class="container py-5 my-md-5">
 	<div class="row align-items-center py-md-5 my-md-5">
-		<div class="col-md-6 col-sm-6 col-12">
-			<h2>Public Opinion Polling</h2>
-			<p>Whether you are interested in a statewide sample or drilling down in Latinos, our firm has expertise in sample, mixed-mode methodology, and helping you understand and analyze public opinion</p>
-			<h2>Message and Ad Testing</h2>
-			<p>After baseline attitudes are established, we can help you communicate most effectively with the target audience through survey experiments and message testing that evaluate how different themes or advertisements resonate for your respondents as a whole, but also within key subgroups within the population</p>
-			<h2>Data Analytics and Modeling</h2>
-			<p>Big data can be overwhelming. Our team of data scientists have pioneered some of the leading methodology and statistical techniques to best assess large datasets and distill them into digestible patterns</p>
-			<h2>Focus Groups and Qualitative</h2>
-			<p class="mb-5 mb-md-0">Whether it is at the start of a new project, or to wrap up a quantitative study, having in-depth conversations with everyday people is crucial to telling the full story and going beyond numbers. We have extensive experience with qualitative research, including focusing in on diverse and underrepresented populations.</p>
+		<div class="col-md-4 col-sm-4 col-12">
+			<h2>Start working with our team today</h2>
+			<p>Interested in polling or message testing research? Focus groups or qualitative interviews? Modeling and data analytics? Contact us today to discuss the possibilities...</p>
+			<ul class="contact-social-links">
+				<li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+				<li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+				<li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+			</ul>
 		</div>
-		<div class="col-md-6 col-sm-6 col-12">
+		<div class="col-md-8 col-sm-8 col-12">
 				<img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/images/services-pageimg.png" alt="bodyimage">
 		</div>
 	</div>
