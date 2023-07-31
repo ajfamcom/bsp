ee47c156013d8f0d1cf79e5766a26513cfc64115
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-31 02:53:37 -0700

    Update contact-us.php

--- wp-content/themes/bspblog/template-parts/contact-us.php
+++ wp-content/themes/bspblog/template-parts/contact-us.php
@@ -44,7 +44,7 @@ die();
 		<div class="col-md-8 col-sm-8 col-12">
 		<div class="container">
          <h2>Contact Us</h2>
-			<form method="POST">
+			<form method="POST" action="<?php echo site_url('contact-us/');?>">
 			<div class="mb-3">
 				<label for="name" class="form-label">Name</label>
 				<input type="text" class="form-control" id="name" name="name" required>
