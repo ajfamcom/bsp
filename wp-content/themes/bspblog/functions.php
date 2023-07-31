8d3db4163482137ab741dba73e4b05a9fa39d92e
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-31 03:38:05 -0700

    Update functions.php

--- wp-content/themes/bspblog/functions.php
+++ wp-content/themes/bspblog/functions.php
@@ -443,9 +443,9 @@ function custom_contact_form() {
     }
     ?>
 
-    <div class="container">
-	<div class="alert alert-success mt-3"><?php echo isset($msg)?$msg:'';?></div>	
+    <div class="container">		
         <h2>Contact Us</h2>
+		<div class="alert alert-success mt-3"><?php echo isset($msg)?$msg:'';?></div>
         <form method="post" action="<?php echo site_url('contact-us/');?>">
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
