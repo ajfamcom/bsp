b75a9d404fb602bbaabc37ec244b401a8e1d1dac
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-07-31 00:40:07 -0700

    Update contact-us.php

--- wp-content/themes/bspblog/template-parts/contact-us.php
+++ wp-content/themes/bspblog/template-parts/contact-us.php
@@ -33,7 +33,32 @@
 			</ul>
 		</div>
 		<div class="col-md-8 col-sm-8 col-12">
-				
+		<div class="container mt-5">
+         <h2>Contact Us</h2>
+			<form>
+			<div class="mb-3">
+				<label for="name" class="form-label">Name</label>
+				<input type="text" class="form-control" id="name" name="name" required>
+			</div>
+			<div class="mb-3">
+				<label for="organization" class="form-label">Organization</label>
+				<input type="text" class="form-control" id="organization" name="organization">
+			</div>
+			<div class="mb-3">
+				<label for="email" class="form-label">Email</label>
+				<input type="email" class="form-control" id="email" name="email" required>
+			</div>
+			<div class="mb-3">
+				<label for="message" class="form-label">Message</label>
+				<textarea class="form-control" id="message" name="message" rows="5" required></textarea>
+			</div>
+			<div class="form-check">
+				<input type="checkbox" class="form-check-input" id="signup" name="signup">
+				<label class="form-check-label" for="signup">Sign me up for email list, promotions, and more</label>
+			</div>
+			<button type="submit" class="btn btn-primary mt-3">Submit</button>
+			</form>
+  </div>
 		</div>
 	</div>
 </div>
