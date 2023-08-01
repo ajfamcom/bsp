7be377f7520b5dc10c3ea14f01444c988354515a
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-01 04:38:51 -0700

    Create in-the-news.php

---
+++ wp-content/themes/bspblog/template-parts/in-the-news.php
@@ -0,0 +1,55 @@
+<?php /**Template Name:In the newss */?>
+<h3 class="small-title">News And Analysis</h3>
+<?php
+$args = array(
+    'post_type' => 'news_analysis',
+    'posts_per_page' => 5,    
+    'meta_query' => array(
+        array(
+            'key' => 'news_status', 
+            'value'   => 'Active', // Serialized value for 'Yes'
+            'compare' => '='
+        ),
+        array(
+            'key' => 'display_on_homepage', 
+            'value'   => 'Yes', // Serialized value for 'Yes'
+            'compare' => '='
+        ),
+    ),
+);
+
+$query = new WP_Query( $args );
+
+
+if ($query->have_posts()) {
+    while ($query->have_posts()) {
+        $query->the_post();
+        $post_id = get_the_ID();
+        $short_desc = get_field('short_description', $post_id);
+        $link_data = get_field('external_link', $post_id);
+        $link='javascript:void(0)';
+        $target="";
+        if($link_data){
+            $link= $link_data;
+            $target='_blank';
+        }
+        ?>
+<div class="news-single-block">
+    <div class="sidebar-img">
+	<?php
+    if (has_post_thumbnail()) {
+    the_post_thumbnail(); 
+    }
+    ?>
+    </div>
+    <p><a href="<?php echo $link;?>" target="<?php echo  $target;?>" ><?php the_title();?></a></p>
+</div>
+<?php
+    }
+ wp_reset_postdata();
+    
+}
+
+// Reset the query to avoid conflicts with other queries
+ 
+?>  
