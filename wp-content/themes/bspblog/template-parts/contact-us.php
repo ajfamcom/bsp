5ebd42f37abf5bd2b34494b376d7fa19f88d013e
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-02 04:18:20 -0700

    changes

--- wp-content/themes/bspblog/template-parts/contact-us.php
+++ wp-content/themes/bspblog/template-parts/contact-us.php
@@ -1,10 +1,12 @@
 <?php
 /* Template Name: contact Template */
-
+$page_id = get_the_ID();
+$full_banner = get_field('full_banner', $page_id);
+$image_over_banner = get_field('image_over_banner', $page_id);
 ?>
 <?php get_header();
 ?>
-<div class="inner-bnr contact-bnr">
+<div class="inner-bnr contact-bnr" style="background-image: linear-gradient(180deg, rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url('<?php echo $full_banner['url']; ?>')">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-8 col-sm-8 col-12">
@@ -16,7 +18,7 @@
 				</div>
 			</div>
 			<div class="col-md-4 col-sm-4 col-12">
-				<img class="img-fluid bnr-simg" src="<?php bloginfo('template_directory'); ?>/assets/images/contact-side-bnrimg.png" alt="side-bnrimg">
+				<img class="img-fluid bnr-simg" src="<?php echo $image_over_banner['url'];?>"  alt="side-bnrimg">
 			</div>
 		</div>
 	</div>
