12cffc77674da12e89506e485057f1718b3491c9
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-02 00:01:49 -0700

    Update single-polls.php

--- wp-content/themes/bspblog/single-polls.php
+++ wp-content/themes/bspblog/single-polls.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * The template for displaying all single posts.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package Astra
- * @since 1.0.0
- */
+/* Template Name:Custom Polls Template  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
