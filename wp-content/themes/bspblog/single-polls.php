bca886b2eed04b07129269e1e01de06b5b3f475c
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-02 00:06:52 -0700

    Update single-polls.php

--- wp-content/themes/bspblog/single-polls.php
+++ wp-content/themes/bspblog/single-polls.php
@@ -1,32 +1,57 @@
 <?php
 /* Template Name:Custom Polls Template  */
+$page_id = get_custom_page_id('team_members', 'our-team');
+$full_banner = get_field('full_banner', $page_id);
+$image_over_banner = get_field('image_over_banner', $page_id);
+if (have_posts()) :
+	while (have_posts()) : the_post();
+		$post_id = get_the_ID();
+		$fullname = get_field('full_name', $post_id);
+		$education = get_field('education', $post_id);
+		$designation = get_field('designation', $post_id);
+		$image = get_field('profile_image', $post_id);
+		$contact_email = get_field('contact_email', $post_id);
+		$contact_phone = get_field('contact_phone', $post_id);
+	endwhile;
+endif;
+?>
+<?php get_header(); ?>
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
+<div class="single-inner-bnr">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="row page-banner">
+					<?php echo get_breadcrumbs(); ?>
+					<div class="page-title">
+						<h3><?php echo get_the_title(); ?></h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
-get_header(); ?>
+<div class="container">
+	<div class="col-md-12 py-5">
+		<div class="row">
 
-<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+			<div class="single-poll col-12">
+				<div class="single-poll-content">
+					<div class="-single-poll-image">
+					<img src="<?php echo $image['url']; ?>" />					
+				    </div>
+					<div class="single-poll-info">
+						<h1 class="poll-title"><?php the_title();?></h1>						
+						<?php the_content(); ?>
+					</div>
+				</div>
+			</div>
 
-	<?php get_sidebar(); ?>
+		</div>
 
-<?php endif ?>
-
-	<div id="primary" <?php astra_primary_class(); ?>>
-
-		<?php astra_primary_content_top(); ?>
-
-		<?php astra_content_loop(); ?>
-
-		<?php astra_primary_content_bottom(); ?>
-
-	</div><!-- #primary -->
-
-<?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
-
-	<?php get_sidebar(); ?>
-
-<?php endif ?>
-
-<?php get_footer(); ?>
+	</div>
+</div>
+<?php
+get_footer();
+?>
