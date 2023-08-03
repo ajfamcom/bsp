594f4ca1a6eb7535f8c87f20eb948ccf532723dc
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-03 00:22:06 -0700

    home page latest bsp

--- wp-content/themes/bspblog/template-parts/most-popular-posts.php
+++ wp-content/themes/bspblog/template-parts/most-popular-posts.php
@@ -4,7 +4,7 @@
 <?php
 
 $args = array(
-    'post_type' => 'post',
+    'post_type' => 'polls',
     'posts_per_page' => 1,    
     'meta_query' => array(
         array(
@@ -52,11 +52,11 @@ if ($query->have_posts()) {
 ?>  
 <?php
 $args = array(
-    'post_type' => 'post',
+    'post_type' => 'polls',
     'posts_per_page' => 1,    
     'meta_query' => array(
         array(
-            'key' => 'is_static_post', 
+            'key' => 'is_sticky_post', 
             'value'   => 'Yes', // Serialized value for 'Yes'
             'compare' => 'LIKE'
         ),
@@ -100,13 +100,18 @@ if ($query->have_posts()) {
 
 
 $args = array(
-    'post_type' => 'post',
+    'post_type' => 'polls',
     'posts_per_page' => 3,
     'orderby' => 'date',
     'order' => 'ASC',    
     'meta_query' => array(
         array(
             'key' => 'is_featured_post', 
+            'value'   => 'No', // Serialized value for 'Yes'
+            'compare' => '='
+        ),
+        array(
+            'key' => 'is_sticky_post', 
             'value'   => 'No', // Serialized value for 'Yes'
             'compare' => '='
         ),
