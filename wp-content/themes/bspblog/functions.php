0b850ccda6b5d01b0678f04f39d6ca65bc876e26
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-03 02:43:46 -0700

    Update functions.php

--- wp-content/themes/bspblog/functions.php
+++ wp-content/themes/bspblog/functions.php
@@ -346,7 +346,7 @@ function custom_posts_search($search, $query) {
 		//$search='';
 		//AND ({$wpdb->posts}.post_content LIKE "%'.$search_term.'%" || {$wpdb->posts}.post_title LIKE "%'.$search_term.'%" || {$wpdb->posts}.post_excerpt LIKE "%'.$search_term.'%")
 			
-			$search .= " AND ({$wpdb->posts}.post_type='post' || {$wpdb->posts}.post_type='polls' || {$wpdb->posts}.post_type='news_analysis' || {$wpdb->posts}.post_type='team_members')";	
+			$search .= " AND ({$wpdb->posts}.post_type='post' || {$wpdb->posts}.post_type='polls' || {$wpdb->posts}.post_type='news_analysis' || {$wpdb->posts}.post_type='team_members' || {$wpdb->posts}.post_type='manage_services')";	
 		
 		return $search;   
 		
