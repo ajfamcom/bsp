757b51e56274b6938277b430e81d41e31552da93
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-03 23:19:36 -0700

    Update header.php

--- wp-content/themes/bspblog/header.php
+++ wp-content/themes/bspblog/header.php
@@ -16,6 +16,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/media-queries.css" type="text/css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+	
+
 	<?php wp_head(); ?>
 <?php astra_head_bottom(); ?>
 <style>
