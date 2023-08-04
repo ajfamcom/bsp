38128a45ec3effe4e39cef34793c604fca335582
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-03 21:53:53 -0700

    Update functions.php

--- wp-content/themes/bspblog/functions.php
+++ wp-content/themes/bspblog/functions.php
@@ -538,7 +538,7 @@ function custom_contact_form() {
         <form method="post" action="<?php echo site_url('contact-us/');?>">
             <div class="mb-3">
                 <!-- <label for="name" class="form-label">Name</label> -->
-                <input type="text" class="form-control" id="fname" name="fname" placeholder="Name*" required>
+                <input type="text" class="form-control" id="fname" name="fname" placeholder="Name*" >
             </div>
             <div class="mb-3">
                 <!-- <label for="organization" class="form-label">Organization</label> -->
