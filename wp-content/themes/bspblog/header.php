ce34f070198fc3e03195ab63df572ed063a51d22
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-04 01:41:30 -0700

    Update header.php

--- wp-content/themes/bspblog/header.php
+++ wp-content/themes/bspblog/header.php
@@ -15,8 +15,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/custom_style.css" type="text/css">
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/media-queries.css" type="text/css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-	
+	<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 	<?php wp_head(); ?>
 <?php astra_head_bottom(); ?>
