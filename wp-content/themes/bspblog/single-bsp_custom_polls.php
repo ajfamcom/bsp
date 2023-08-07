0698ad5f28eae7d9d5dfb336172e89488fadb3ff
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-07 03:01:30 -0700

    changes

--- wp-content/themes/bspblog/single-bsp_custom_polls.php
+++ wp-content/themes/bspblog/single-bsp_custom_polls.php
@@ -38,7 +38,7 @@ if ($metadata) {
 					while (have_posts()) : the_post();
 					    $post_id=get_the_ID();
 						$download_attachment=get_field('pdf_attachment',$post_id);
-						$metaData=get_pdf_metadata_by_post($post_id);
+						$metaData=get_pdf_metadata_custom($post_id);
 						/*$metaData = get_pdf_prop($download_attachment['url']);*/
 						if ($metadata) {							
 							print($metadata);							
