ed73994c760b9033732627f5f7e9597daa27a0f1
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-08 04:10:08 -0700

    Update footer.php

--- wp-content/themes/bspblog/footer.php
+++ wp-content/themes/bspblog/footer.php
@@ -55,11 +55,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php   
 	wp_footer(); 
 ?>
-<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.11.0/css/bootstrap-datepicker.min.css" rel="stylesheet">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.11.0/js/bootstrap-datepicker.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 
 <script>
 	$(document).ready(function(){
@@ -76,12 +75,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 	})
 </script>
 <script>
-	$(document).ready(function($) {
+	/* $(document).ready(function($) {
     $('.datepicker').datepicker({
         format: 'yyyy-mm-dd', // Set the desired date format
         autoclose: true // Close the datepicker when a date is selected
     });
-});
+}); */
 
 </script>
 	</body>
