4b7c66b4d40037f576ac7f87c6346ea079f31909
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-08 04:11:47 -0700

    Update footer.php

--- wp-content/themes/bspblog/footer.php
+++ wp-content/themes/bspblog/footer.php
@@ -59,7 +59,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
-
 <script>
 	$(document).ready(function(){
 		if (window.innerWidth < 768) {
@@ -73,15 +72,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 		$("#menu-primary li").slice(-2).show();
 		}
 	})
-</script>
-<script>
-	/* $(document).ready(function($) {
-    $('.datepicker').datepicker({
-        format: 'yyyy-mm-dd', // Set the desired date format
-        autoclose: true // Close the datepicker when a date is selected
-    });
-}); */
-
 </script>
 	</body>
 </html>
