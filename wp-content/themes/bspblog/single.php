901ed061f3f8aaba0fda2a7ab2be6130877080e2
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-09 02:12:59 -0700

    Update single.php

--- wp-content/themes/bspblog/single.php
+++ wp-content/themes/bspblog/single.php
@@ -14,26 +14,100 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header(); ?>
 
-<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+<div class="single-inner-bnr">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="row page-banner">
+					<?php echo get_breadcrumbs(); ?>
+					<div class="page-title">
+						<h3><?php echo get_the_title(); ?></h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
-	<?php get_sidebar(); ?>
+<div class="container">
 
-<?php endif ?>
+	<div class="col-md-12 py-5">
+		<div class="row">
+					<?php 
+					while (have_posts()) : the_post();
+					    $post_id=get_the_ID();
+						$download_attachment=get_field('pdf_attachment',$post_id);
+						//$metaData=get_pdf_metadata_custom($post_id);
+						 $pdfkey=get_post_meta( $post_id, 'custom_pdf_keywords' );
+						 print_r($pdfkey);
+					?>
+				<div class="single-poll col-12">
+						<div class="single-poll-content">
+							<div class="-single-poll-image">
+							<img src="<?php echo $image['url']; ?>" />					
+							</div>
+							<div class="single-poll-info">
+								<h1 class="poll-title"><?php the_title();?></h1>						
+								<?php the_content(); ?>	
+														
+								<?php if($download_attachment):?>
+								<p><a href="<?php echo esc_url($download_attachment['url']); ?>" target="_blank" rel="nofollow">Download PDF</a></p>
+								<?php endif; ?>
+							</div>
+							
+						</div>
+						<div>
+						<?php 
+						//$pdf_file_path = 'path/to/your/pdf/file.pdf';
+						
+						/*if ($metadata) {
+							// Process and display the metadata as needed
+							$title = $metadata->get('Title');
+							$author = $metadata->get('Author');
+							$keywords = $metadata->get('Keywords');
+							// ... Add more metadata properties as required
+						
+							echo "Title: $title<br>";
+							echo "Author: $author<br>";
+							echo "Keywords: $keywords<br>";
+							// ... Output more metadata properties as needed
+						} else {
+							echo "No metadata found in the PDF.";
+						}*/
+						?></div>
+						<div class="tags">
+								<?php
+									$tags = get_the_terms(get_the_ID(), 'post_tag');
+									if ($tags && !is_wp_error($tags)) {
+										echo '<ul class="poll-tags">';
+										foreach ($tags as $tag) {
+											echo '<li><a href="' . esc_url(get_term_link($tag)) . '">' . esc_html($tag->name) . '</a></li>';
+										}
+										echo '</ul>';
+									}
+									?>
+						</div>
+						<div class="comments-section">
+									<?php if (comments_open() || get_comments_number()) : ?>
+										<h2><?php comments_number('No Comments', '1 Comment', '% Comments'); ?></h2>
+										<?php
+										if (comments_open()) {																
+																					
+											comment_form();
+											
+										} else {
+											echo '<p>Comments are closed.</p>';
+										}
+										?>
+									<?php endif; ?>
+                        </div>
 
-	<div id="primary" <?php astra_primary_class(); ?>>
-        <?php echo "testing123";?>
-		<?php astra_primary_content_top(); ?>
+			    </div>
+                    <?php endwhile; ?>
+		</div>
 
-		<?php astra_content_loop(); ?>
-
-		<?php astra_primary_content_bottom(); ?>
-
-	</div><!-- #primary -->
-
-<?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
-
-	<?php get_sidebar(); ?>
-
-<?php endif ?>
-
-<?php get_footer(); ?>
+	</div>
+</div>
+<?php
+get_footer();
+?>
