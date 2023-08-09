3b6d6966734ff6eee070b1fe877cfd733dd8d0b0
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-09 03:40:48 -0700

    Update single-bsp_custom_polls.php

--- wp-content/themes/bspblog/single-bsp_custom_polls.php
+++ wp-content/themes/bspblog/single-bsp_custom_polls.php
@@ -64,6 +64,16 @@
 	</div>
 	<div class="col-md-12 py-5">
 		<h3>Related Posts</h3>
+		<section class="splide" aria-labelledby="carousel-heading">
+        <h2 id="carousel-heading">Splide Basic HTML Example</h2>
+			<div class="splide__track">
+					<ul class="splide__list">
+						<li class="splide__slide">Slide 01</li>
+						<li class="splide__slide">Slide 02</li>
+						<li class="splide__slide">Slide 03</li>
+					</ul>
+			</div>
+        </section>
 	</div>
 </div>
 <?php
