42ec1fc700054e18185a25448e9c94b2d5030cd2
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-09 03:38:38 -0700

    single polls

--- wp-content/themes/bspblog/single.php
+++ wp-content/themes/bspblog/single.php
@@ -76,6 +76,16 @@ get_header(); ?>
 	</div>
 	<div class="col-md-12 py-5">
 		<h3>Related Posts</h3>
+		<section class="splide" aria-labelledby="carousel-heading">
+        <h2 id="carousel-heading">Splide Basic HTML Example</h2>
+			<div class="splide__track">
+					<ul class="splide__list">
+						<li class="splide__slide">Slide 01</li>
+						<li class="splide__slide">Slide 02</li>
+						<li class="splide__slide">Slide 03</li>
+					</ul>
+			</div>
+        </section>
 	</div>
 </div>
 <?php
