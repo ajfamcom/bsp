639e0b44061ff63c57c586579cdf86923ab07d59
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-10 00:08:02 -0700

    Update single.php

--- wp-content/themes/bspblog/single.php
+++ wp-content/themes/bspblog/single.php
@@ -136,7 +136,7 @@ $query = "
     AND {$wpdb->prefix}posts.post_status='publish'
     AND (
         ({$wpdb->prefix}postmeta.meta_key = 'custom_pdf_keywords' AND {$wpdb->prefix}postmeta.meta_value IN (" . implode(',', $break_search_text) . "))
-	    ({$wpdb->prefix}postmeta.meta_key = 'related_post_keywords' AND {$wpdb->prefix}postmeta.meta_value LIKE %s)
+	    OR ({$wpdb->prefix}postmeta.meta_key = 'related_post_keywords' AND {$wpdb->prefix}postmeta.meta_value LIKE %s)
 		OR ({$wpdb->prefix}postmeta.meta_key = 'related_polls_keywords' AND {$wpdb->prefix}postmeta.meta_value LIKE %s) 
 		OR {$wpdb->prefix}posts.post_title LIKE %s                       
     )            
