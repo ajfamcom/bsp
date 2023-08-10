a645d07f18e3ac1ccdfe651e4053afc58caa6b7b
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-09 22:10:36 -0700

    Update single.php

--- wp-content/themes/bspblog/single.php
+++ wp-content/themes/bspblog/single.php
@@ -82,7 +82,7 @@ get_header(); ?>
 				<div class="single-poll-content">
 						<div class="single-poll-info">
 							<h1 class="poll-title"><?php the_title(); ?></h1>
-							<p>WLRN 91.3 FM | By <?php echo $author_name;?>,Published <?php echo $post_date; ?></p>
+							<p>WLRN 91.3 FM | By <?php echo $author_name;?> , Published <?php echo $post_date; ?></p>
 							<?php the_content(); ?>
 							<p>
                               <?php if($download) { ?>
@@ -110,55 +110,40 @@ get_header(); ?>
 		<h2 id="carousel-heading text-center">Related Posts</h2>
 		<div class="splide__track">
 			<ul class="splide__list">
-				<?php
-				$fargs = array(
-					'post_type'      => 'bsp_custom_polls',
-					'posts_per_page' => -1,
-					'meta_query'     => array(
-						array(
-							'key'     => 'is_featured_poll',
-							'value'   => 'No',
-							'compare' => '='
-						),
-					),
-				);
-
-				$fquery = new WP_Query($fargs);
-
-				if ($fquery->have_posts()) :
-					while ($fquery->have_posts()) :
-						$fquery->the_post();
-						$post_id = get_the_ID();
-
-						$permalink = get_permalink($post_id);
-						if (has_post_thumbnail($post_id)) {
-
-							$thumbnail_id = get_post_thumbnail_id($post_id);
-							$image_url = wp_get_attachment_url($thumbnail_id);
-							$theme_directory_uri = get_template_directory_uri();
-							$noimage = $theme_directory_uri . '/assets/images/on-image-placeholder.jpg';
-
-							$image_link = '<img src="' . esc_url($image_url) . '" alt="Featured Image" class="img-fluid">';
-						} else {
-							$image_link = '<img src="' . esc_url($noimage) . '" alt="Featured Image" class="img-fluid">';
-						}
-				?>
+				
 						<li class="splide__slide">
 							<div class="news-block col-md-4">
-								<div class="news-image"><?php echo $image_link; ?></div>
+								<div class="news-image"><img src="https://bsp.thefamcomlab.com/wp-content/uploads/2023/07/imagefive.jpg"></div>
 								<div class="news-info">
-									<h4 class="news-details"><span class="news-title"><?php the_title(); ?></span></h4>
-									<p class="news-other-details"><span class="news-date"><?php echo get_the_date('M j, Y'); ?></span></p>
-									<p class="news-content"><?php the_content(); ?></p>
-									<p><a href="<?php echo $permalink; ?>">Read More</a></p>
+									<h4 class="news-details"><span class="news-title">Title here</span></h4>
+									<p class="news-other-details"><span class="news-date"><?php echo date('M j, Y'); ?></span></p>
+									<p class="news-content">Content here Content here Content hereContent hereContent hereContent here</p>
+									<p><a href="https://www.google.com">Read More</a></p>
 								</div>
 							</div>
 						</li>
-				<?php
-					endwhile;
-					wp_reset_postdata();
-				endif;
-				?>
+						<li class="splide__slide">
+							<div class="news-block col-md-4">
+								<div class="news-image"><img src="https://bsp.thefamcomlab.com/wp-content/uploads/2023/07/imagefive.jpg"></div>
+								<div class="news-info">
+									<h4 class="news-details"><span class="news-title">Title here</span></h4>
+									<p class="news-other-details"><span class="news-date"><?php echo date('M j, Y'); ?></span></p>
+									<p class="news-content">Content here Content here Content hereContent hereContent hereContent here</p>
+									<p><a href="https://www.google.com">Read More</a></p>
+								</div>
+							</div>
+						</li>
+						<li class="splide__slide">
+							<div class="news-block col-md-4">
+								<div class="news-image"><img src="https://bsp.thefamcomlab.com/wp-content/uploads/2023/07/imagefive.jpg"></div>
+								<div class="news-info">
+									<h4 class="news-details"><span class="news-title">Title here</span></h4>
+									<p class="news-other-details"><span class="news-date"><?php echo date('M j, Y'); ?></span></p>
+									<p class="news-content">Content here Content here Content hereContent hereContent hereContent here</p>
+									<p><a href="https://www.google.com">Read More</a></p>
+								</div>
+							</div>
+						</li>
 			</ul>
 		</div>
 	</section>
