164d199c2091a75163cde9dbffe6b8fdfa37da92
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-10 05:40:40 -0700

    Update single-bsp_custom_polls.php

--- wp-content/themes/bspblog/single-bsp_custom_polls.php
+++ wp-content/themes/bspblog/single-bsp_custom_polls.php
@@ -129,12 +129,22 @@ $moredata ='';
 
 
 $query = "SELECT wp_posts.ID, wp_posts.post_title, wp_posts.post_content, wp_posts.post_date
-    FROM {$wpdb->prefix}posts
-    INNER JOIN {$wpdb->prefix}postmeta ON {$wpdb->prefix}posts.ID = {$wpdb->prefix}postmeta.post_id
-    WHERE {$wpdb->prefix}posts.post_type = 'bsp_custom_polls'
-    AND {$wpdb->prefix}posts.post_status = 'publish' 
-	GROUP BY {$wpdb->prefix}posts.ID
-    ORDER BY {$wpdb->prefix}posts.post_date DESC
+FROM wp_posts
+INNER JOIN wp_postmeta ON wp_posts.ID = wp_postmeta.post_id
+WHERE wp_posts.post_type = 'post'
+AND wp_posts.post_status = 'publish'
+AND (
+    wp_postmeta.meta_key = 'custom_pdf_keywords'
+    AND (
+        wp_postmeta.meta_value LIKE '%Arizona%'
+        OR wp_postmeta.meta_value LIKE '%2022%'
+        OR wp_postmeta.meta_value LIKE '%midterm%'
+        OR wp_postmeta.meta_value LIKE '%statewide%'
+    )
+)
+GROUP BY wp_posts.ID
+ORDER BY wp_posts.post_date DESC;
+
 ";
 echo $query;
 $results = $wpdb->get_results($query); 
