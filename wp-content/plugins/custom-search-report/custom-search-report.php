4c670680fd620b292b510a8f5273d60ffc4f8a25
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-10 22:39:15 -0700

    Create custom-search-report.php

---
+++ wp-content/plugins/custom-search-report/custom-search-report.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name: Custom Search report
+ * Description: Plugin for search report.
+ * Version: 1.0.0
+ * Author: Famcom
+ * Author URI: https://famcom.com
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Invalid request.' );
+}
+
+define('CUSTOMCONTENT_PLUGINPATH',plugin_dir_path( __FILE__ ));
+define('CUSTOMCONTENT_PLUGINURI',plugin_dir_url( __FILE__ ));
+
+
+
+// Add a menu item to the admin menu
+function custom_data_display_menu() {
+    add_menu_page(
+        'Search Report Display',
+        'Search report Data',
+        'manage_options',
+        'search-report-display',
+        'search_report_data_display_page'
+    );
+}
+add_action('admin_menu', 'custom_data_display_menu');
+
+function custom_data_display_page() {
+    
+    $data = $wpdb->get_results("SELECT * FROM your_custom_table");
+   
+    echo '<div class="wrap">';
+    echo '<h2>Custom Data Display</h2>';   
+    
+    if (!empty($data)) {
+        foreach ($data as $item) {            
+            echo '<p>' . $item->column_name . '</p>';
+        }
+    } else {
+        echo '<p>No data found.</p>';
+    }
+
+    echo '</div>';
+}
+
