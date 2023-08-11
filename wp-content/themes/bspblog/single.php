e506e67fae5fc96b50184fba7726085e8659fb78
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-10 21:36:19 -0700

    Update single.php

--- wp-content/themes/bspblog/single.php
+++ wp-content/themes/bspblog/single.php
@@ -126,25 +126,25 @@ if ($breakcode) {
 $search_text = 'Hispanic';//$search['Keywords'];
 $break_search_text = array(); // Initialize the array
 
-if (strpos($search_text, ' ') !== false) {
-    $break_search_text = explode(' ', $search_text);
-} else {
-    $break_search_text[] = $search_text;
-}
-//({$wpdb->prefix}postmeta.meta_key = 'custom_pdf_keywords' AND {$wpdb->prefix}postmeta.meta_value IN (" . implode(',', $break_search_text) . "))
-//OR ({$wpdb->prefix}postmeta.meta_key = 'related_post_keywords' AND {$wpdb->prefix}postmeta.meta_value LIKE %s)
-//OR ({$wpdb->prefix}postmeta.meta_key = 'related_polls_keywords' AND {$wpdb->prefix}postmeta.meta_value LIKE %s) 
 $query = "
-    SELECT {$wpdb->prefix}posts.ID, {$wpdb->prefix}posts.post_title, {$wpdb->prefix}posts.post_content, {$wpdb->prefix}posts.post_date ,{$wpdb->prefix}posts.post_status='publish'
+    SELECT wp_posts.ID, wp_posts.post_title, wp_posts.post_content, wp_posts.post_date
     FROM {$wpdb->prefix}posts
-    LEFT JOIN {$wpdb->prefix}postmeta ON ({$wpdb->prefix}posts.ID = {$wpdb->prefix}postmeta.post_id)
+    INNER JOIN {$wpdb->prefix}postmeta ON {$wpdb->prefix}posts.ID = {$wpdb->prefix}postmeta.post_id
     WHERE ({$wpdb->prefix}posts.post_type = 'bsp_custom_polls' OR {$wpdb->prefix}posts.post_type = 'post')
-    AND {$wpdb->prefix}posts.post_status='publish'    
-    GROUP BY {$wpdb->prefix}posts.ID, {$wpdb->prefix}posts.post_title, {$wpdb->prefix}posts.post_content, {$wpdb->prefix}posts.post_date
-    ORDER BY {$wpdb->prefix}posts.post_date DESC
-";
-//$query = $wpdb->prepare($query, '%' . $wpdb->esc_like($search_text) . '%');
+    AND {$wpdb->prefix}posts.post_status = 'publish' ";
 
+$query .= " AND (
+    wp_postmeta.meta_key = 'custom_pdf_keywords'
+    AND (
+        wp_postmeta.meta_value LIKE '%xxx%' ";
+
+    $query .= $addData;
+
+ $query .= ")
+)";
+
+$query .= " GROUP BY {$wpdb->prefix}posts.ID,{$wpdb->prefix}posts.post_title,{$wpdb->prefix}posts.post_content,{$wpdb->prefix}posts.post_date
+    ORDER BY {$wpdb->prefix}posts.post_date DESC";
 
 $results = $wpdb->get_results($query);
 		
