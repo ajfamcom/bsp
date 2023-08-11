f6032dcc22042e24f23a4255951ccc371160807a
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-11 01:45:33 -0700

    Update custom-search-template.php

--- wp-content/plugins/custom-search-report/custom-search-template.php
+++ wp-content/plugins/custom-search-report/custom-search-template.php
@@ -27,13 +27,13 @@ $data = $wpdb->get_results("SELECT * FROM wp_searchdata");
     </thead>
     <tbody id="">
     <?php
-    print_r($data);
+    
      if (!empty($data)) {
         foreach ($data as $item) {            
-            echo '<tr><td>Keyword:</td><td>' . $item->keyword . '</td></tr>';
-			echo '<tr><td>Visitor IP:</td><td>' . $item->visitor_ip . '</td></tr>';
-			echo '<tr><td>Search DateTime:</td><td>' . $item->created_at . '</td></tr>';
-			echo '<tr><td>Search From:</td><td>' . $item->search_page . '</td></tr>';
+      echo '<tr><td>Keyword:</td><td>' .$item->keyword . '</td></tr>';
+			echo '<tr><td>Visitor IP:</td><td>' .$item->visitor_ip . '</td></tr>';
+			echo '<tr><td>Search DateTime:</td><td>' .$item->created_at . '</td></tr>';
+			echo '<tr><td>Search From:</td><td>' .$item->search_page . '</td></tr>';
         }
     } else {
         echo '<tr><td>No data found.</td></tr>';
