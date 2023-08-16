6af09b811ef14627807c99587677a88a3d5c2441
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-16 04:31:30 -0700

    Update services-page.php

--- wp-content/themes/bspblog/template-parts/services-page.php
+++ wp-content/themes/bspblog/template-parts/services-page.php
@@ -30,7 +30,7 @@ $image_over_banner = get_field('image_over_banner', $page_id);
 $args = array(
     'post_type' => 'manage_services',
     'posts_per_page' => 4,
-    'meta_key'       => 'services_sort_order', // New parameter for sorting
+	'meta_key'       => 'services_sort_order', // New parameter for sorting
     'orderby'        => 'meta_value_num',    // Sort by numeric value of meta_key
     'order'          => 'ASC',              // Ascending order
 );
