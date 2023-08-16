1493f40160ca5763e5f41f5cbb65f73d2dd4cac7
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-15 23:49:32 -0700

    Update single-bsp_custom_polls.php

--- wp-content/themes/bspblog/single-bsp_custom_polls.php
+++ wp-content/themes/bspblog/single-bsp_custom_polls.php
@@ -24,7 +24,7 @@ $permalink = get_permalink($post_id);
 				$search=get_pdf_metadata_custom($post_id);
 
 				$multiple_pdf_attachment=get_field('multiple_pdf_attachment',$post_id);
-				print_r($multiple_pdf_attachment);
+				echo '<pre>';print_r($multiple_pdf_attachment);
 
 endwhile;
 
