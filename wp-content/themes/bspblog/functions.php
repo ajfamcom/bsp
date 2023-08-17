18c65cb263bb8d5a48670aac382ea29baf0d35be
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-16 23:59:11 -0700

    Update functions.php

--- wp-content/themes/bspblog/functions.php
+++ wp-content/themes/bspblog/functions.php
@@ -804,7 +804,7 @@ function get_multiple_pdf_metadata_custom($postid,$type='polls') {
         
            // Retrieve attachment metadata and perform actions
            $attachment_id = $attachment->ID;
-           delete_tags_when_attachment_removed(($attachment_id));
+           //delete_tags_when_attachment_removed(($attachment_id));
            $attachment_url = wp_get_attachment_url($attachment_id);
            $file_path='/var/www/html/bsp'.wp_make_link_relative( $attachment_url);
                 $parser = new \Smalot\PdfParser\Parser();
