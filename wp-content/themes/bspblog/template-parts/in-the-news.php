d258daa36d9591855dea057ac4e12000aaa9dbde
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-18 03:08:12 -0700

    Update in-the-news.php

--- wp-content/themes/bspblog/template-parts/in-the-news.php
+++ wp-content/themes/bspblog/template-parts/in-the-news.php
@@ -23,15 +23,16 @@ $image_over_banner = get_field('image_over_banner', $page_id);
 		</div>
 	</div>
 </div>
-
-
+<div class="container py-5 my-md-5">	
+<div class="d-flex align-items-center gap-3 mb-4">
+    <a href="#." onclick="show('grid');"><i style="background-color: #153d67;" class="fa-solid fa-grip fs-2 rounded-3 text-white py-1 px-2"></i></a>
+    <a href="#." onclick="show('list');"><i style="background-color: #153d67;" class="fa-solid fa-grip-lines fs-2 rounded-3 text-white py-1 px-2"></i></a>
+</div>
+</div>
 <div class="container grid-post py-5 my-md-5">	
 	<div class="col-md-12 py-md-5">
 
-    <div class="d-flex align-items-center gap-3 mb-4">
-    <a href="#." onclick="show('grid');"><i style="background-color: #153d67;" class="fa-solid fa-grip fs-2 rounded-3 text-white py-1 px-2"></i></a>
-    <a href="#." onclick="show('list');"><i style="background-color: #153d67;" class="fa-solid fa-grip-lines fs-2 rounded-3 text-white py-1 px-2"></i></a>
-    </div>
+   
 
 		<div class="row">
 			<?php
