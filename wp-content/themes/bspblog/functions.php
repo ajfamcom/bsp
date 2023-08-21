2fb202ae2eaeb941619deec3ef0841376b0a30ec
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-08-20 23:16:10 -0700

    Update functions.php

--- wp-content/themes/bspblog/functions.php
+++ wp-content/themes/bspblog/functions.php
@@ -890,11 +890,11 @@ function save_pdf_meta($post_id) {
             }      
         
                 $existing_tags = wp_get_post_tags($post_id, array('fields' => 'names','status'=>'published')); 
-                if($existing_tags)  
-                {
+                //if($existing_tags)  
+                //{
                     $combined_tags = array_unique(array_merge($existing_tags, $keywordsArray));
                     wp_set_post_tags($post_id, $combined_tags, false);
-                }              
+                //}              
                
                 
             } 
