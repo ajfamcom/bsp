de51761671714ef9aa51e1cce442f188cf6ae739
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-09-18 01:54:39 -0700

    Update bspblog-config.php

--- wp-content/themes/bspblog/mailchimpsettings/bspblog-config.php
+++ wp-content/themes/bspblog/mailchimpsettings/bspblog-config.php
@@ -1,5 +1,5 @@
 <?php
-define('MAILCHIMP_API_KEY', '6486b38910678df8eccaf6b7c90cbad0-us13');
+define('MAILCHIMP_API_KEY', '532897c8aeddb24b94673ee0825cfdd5-us13');
 define('MAILCHIMP_ID', '1407967842');
 // Add more configuration options as needed...
 ?>
