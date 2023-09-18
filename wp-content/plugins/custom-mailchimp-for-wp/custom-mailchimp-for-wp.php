4bf8b6817b582542aa719076b58a539ead2137ea
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-09-18 01:52:15 -0700

    changes in contact

---
+++ wp-content/plugins/custom-mailchimp-for-wp/custom-mailchimp-for-wp.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Custom Mailchimp for Wp
+ * Description: Plugin Extended.
+ * Version: 1.0.0
+ * Author: Famcom
+ * Author URI: https://famcom.com
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Invalid request.' );
+}
+
