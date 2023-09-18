d7b8467606c979383d1ceaf6615c417863c617d6
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-09-17 21:13:25 -0700

    Update functions.php

--- wp-content/themes/bspblog/functions.php
+++ wp-content/themes/bspblog/functions.php
@@ -504,12 +504,12 @@ function custom_contact_form() {
     if(!empty($name) && !empty($email) && !empty($message)){
         $wpdb->insert( $table_name, $data );
         //wp_mail('dipti@famcominc.com', 'Test Email', 'This is a test email from WordPress.');
-        echo 'halo'.$is_signup;die();
+        
        
         wp_mail( $to, $subject, $all_message, $headers );  
         wp_mail( $user_to, $user_subject, $user_message, $user_headers );      
         $msg="Thank you for your inquiry! We will get back to you within 48 hours.We've sent you a confirmation email, please click the link to verify your address.";
-        if(isset($is_signup)){
+        if($is_signup!='No'){
             @add_or_update_member('0e2a3b129f',$email,$name);
         }
     }
