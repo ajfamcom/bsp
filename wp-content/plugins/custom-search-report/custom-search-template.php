28a48693b4b405557da3bc6f99b56298097ffacd
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-09-25 01:30:55 -0700

    Update custom-search-template.php

--- wp-content/plugins/custom-search-report/custom-search-template.php
+++ wp-content/plugins/custom-search-report/custom-search-template.php
@@ -9,7 +9,7 @@ $current_admin_url = add_query_arg(array('page' => 'search-report-display'), $cu
 // Add this code to your existing PHP code, after the form submission check.
 $start_date = isset($_GET['start_date']) ? sanitize_text_field($_GET['start_date']) : '';
 $end_date = isset($_GET['end_date']) ? sanitize_text_field($_GET['end_date']) : '';
-echo $search_page = isset($_GET['search_page']) ? sanitize_text_field($_GET['search_page']) : '';
+$search_page = isset($_GET['search_page']) ? sanitize_text_field($_GET['search_page']) : '';
 $query = "SELECT 
 keyword,
 visitor_ip,
@@ -18,7 +18,7 @@ search_page
 FROM wp_searchdata WHERE 1=1";
 
 if (!empty($search_page)) {
-    $query .= " AND search_page LIKE '$search_page'";
+    echo $query .= " AND search_page LIKE '$search_page'";die();
 }
 
 if (!empty($start_date) && !empty($end_date)) {
