d438048a1b8c632c273d238fb51ffad755b02e40
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-09-24 21:56:29 -0700

    Update custom-search-template.php

--- wp-content/plugins/custom-search-report/custom-search-template.php
+++ wp-content/plugins/custom-search-report/custom-search-template.php
@@ -123,16 +123,7 @@ $total_pages = ceil($total_items / $items_per_page);
     <button type="button" class="custom-search-button btn btn-primary">Total Result Count: <b><?php echo $total_items;?></b></button>
    
 </form>
-<table border="0" cellspacing="5" cellpadding="5">
-        <tbody><tr>
-            <td>Minimum date:</td>
-            <td><input type="text" id="min" name="min"></td>
-        </tr>
-        <tr>
-            <td>Maximum date:</td>
-            <td><input type="text" id="max" name="max"></td>
-        </tr>
-    </tbody></table>
+
   <table class="table table-striped table-bordered custom-search-table" id="sortTable">
     <thead>
       <tr>
@@ -218,42 +209,6 @@ $total_pages = ceil($total_items / $items_per_page);
           
         });
     </script>
-    <script>
-        let minDate, maxDate;
- 
- // Custom filtering function which will search data in column four between two values
- DataTable.ext.search.push(function (settings, data, dataIndex) {
-     let min = minDate.val();
-     let max = maxDate.val();
-     let date = new Date(data[2]);
-  
-     if (
-         (min === null && max === null) ||
-         (min === null && date <= max) ||
-         (min <= date && max === null) ||
-         (min <= date && date <= max)
-     ) {
-         return true;
-     }
-     return false;
- });
-  
- // Create date inputs
- minDate = new DateTime('#min', {
-     format: 'MMMM Do YYYY'
- });
- maxDate = new DateTime('#max', {
-     format: 'MMMM Do YYYY'
- });
-  
- // DataTables initialisation
- let table = new DataTable('#sortTable');
-  
- // Refilter the table
- document.querySelectorAll('#min, #max').forEach((el) => {
-     el.addEventListener('change', () => table.draw());
- });
-    </script>    
 
 
 
