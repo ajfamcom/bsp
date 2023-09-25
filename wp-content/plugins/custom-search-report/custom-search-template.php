1b4c1129ff064cdd9275fd75c4a5328ba7a0ea0e
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-09-24 21:53:20 -0700

    Update custom-search-template.php

--- wp-content/plugins/custom-search-report/custom-search-template.php
+++ wp-content/plugins/custom-search-report/custom-search-template.php
@@ -209,6 +209,42 @@ $total_pages = ceil($total_items / $items_per_page);
           
         });
     </script>
+    <script>
+        let minDate, maxDate;
+ 
+ // Custom filtering function which will search data in column four between two values
+ DataTable.ext.search.push(function (settings, data, dataIndex) {
+     let min = minDate.val();
+     let max = maxDate.val();
+     let date = new Date(data[2]);
+  
+     if (
+         (min === null && max === null) ||
+         (min === null && date <= max) ||
+         (min <= date && max === null) ||
+         (min <= date && date <= max)
+     ) {
+         return true;
+     }
+     return false;
+ });
+  
+ // Create date inputs
+ minDate = new DateTime('#min', {
+     format: 'MMMM Do YYYY'
+ });
+ maxDate = new DateTime('#max', {
+     format: 'MMMM Do YYYY'
+ });
+  
+ // DataTables initialisation
+ let table = new DataTable('#sortTable');
+  
+ // Refilter the table
+ document.querySelectorAll('#min, #max').forEach((el) => {
+     el.addEventListener('change', () => table.draw());
+ });
+    </script>    
 
 
 
