0ba89fcc3cc93b896ad2fe49cd0d8b83f5ac7aad
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-09-25 03:14:42 -0700

    changes

--- wp-content/plugins/custom-search-report/custom-search-report.php
+++ wp-content/plugins/custom-search-report/custom-search-report.php
@@ -135,6 +135,7 @@ function remove_core_updates() {
         'version_checked' => $wp_version,
     );
 }
+
 add_filter('pre_site_transient_update_core', 'remove_core_updates');
 add_filter('pre_site_transient_update_plugins', 'remove_core_updates');
 add_filter('pre_site_transient_update_themes', 'remove_core_updates');
