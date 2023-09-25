f2975e5893772cac071ddd059613e0ad73316ef4
dipti108 <122957924+dipti108@users.noreply.github.com>
2023-09-25 03:13:36 -0700

    Update custom-search-report.php

--- wp-content/plugins/custom-search-report/custom-search-report.php
+++ wp-content/plugins/custom-search-report/custom-search-report.php
@@ -128,3 +128,13 @@ add_action('wp_ajax_custom_csv_download', 'custom_csv_download_ajax');
 add_action('wp_ajax_nopriv_custom_csv_download', 'custom_csv_download_ajax');
 
 
+function remove_core_updates() {
+    global $wp_version;
+    return (object) array(
+        'last_checked' => time(),
+        'version_checked' => $wp_version,
+    );
+}
+add_filter('pre_site_transient_update_core', 'remove_core_updates');
+add_filter('pre_site_transient_update_plugins', 'remove_core_updates');
+add_filter('pre_site_transient_update_themes', 'remove_core_updates');
